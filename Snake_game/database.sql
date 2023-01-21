b16664e6a70760e4b442255e718831a74c57c5b9
Akarsh-Hegde <hegdeakarsh2002@gmail.com>
2023-01-21 23:40:31 +0530

    try modification

--- Snake_game/database.sql
+++ Snake_game/database.sql
@@ -1,7 +1,8 @@
-/* trying to add database to the game*/
+/* trying to add database to the game  making a brach add_database on git*/
 
 /*single column table*/
 
 create table score_board(
     player_name varchar(35) not null
+    asdfklhsdf
 );
